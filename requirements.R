a385c3d8bf69857ad31cf9f3e5caf34c7d5d2b34
saqeeb360 <saqeeb.360@gmail.com>
2024-07-03 10:18:08 +0530

    update requirements.R

---
+++ requirements.R
@@ -0,0 +1,8 @@
+# requirements.R
+install.packages("lidR", version = "4.0.3", dependencies = TRUE, repos = "http://cran.rstudio.com/")
+install.packages("rlas", version = "1.6.3", dependencies = TRUE, repos = "http://cran.rstudio.com/")
+install.packages("rgdal", version = "1.6.7", dependencies = TRUE, repos = "http://cran.rstudio.com/")
+install.packages("tictoc", version = "1.2", dependencies = TRUE, repos = "http://cran.rstudio.com/")
+install.packages("sp", version = "2.0.0", dependencies = TRUE, repos = "http://cran.rstudio.com/")
+install.packages("concaveman", version = "1.1.0", dependencies = TRUE, repos = "http://cran.rstudio.com/")
+install.packages("raster", version = "3.6.23", dependencies = TRUE, repos = "http://cran.rstudio.com/")
